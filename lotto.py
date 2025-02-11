fcbbf2df72c3de28f44e6a6801ac90d178e6a31d
lilycato <146873958+lilycato@users.noreply.github.com>
2025-02-11 14:49:42 +0800

    Update lotto.py
    added ability to save generated output

--- lotto.py
+++ lotto.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 from sys import exit
 import random
 
@@ -29,12 +30,30 @@ def lotto_gen_6():
     pick.sort()
     print(pick)
     list_box.insert(tk.END, pick)
+    
+def save_to_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+    if file_path:
+        try:
+            with open(file_path, 'w') as file:
+                list_box_str = str(list_box.get(0, "end"))
+                file.write(list_box_str)
+            status_label.config(text=f"File saved: {file_path}")
+        except Exception as e:
+            status_label.config(text=f"Error saving file: {str(e)}")
+
 
 button_7 = tk.Button(root, text="Generate 7 numbers", command = lotto_gen_7)
 button_7.place(relx=0.3, rely=0.5, anchor='center')
 
 button_6 = tk.Button(root, text="Generate 6 numbers", command = lotto_gen_6)
 button_6.place(relx=0.3, rely=0.6, anchor='center')
+
+save_button = tk.Button(root, text="Save to file", command = save_to_file)
+save_button.place(relx=0.3, rely=0.7, anchor='center')
+
+status_label = tk.Label(root, text="", padx=20, pady=10)
+status_label.pack()
 
 
 root.bind("<Tab>", exit)
