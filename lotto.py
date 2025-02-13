f6e395fb0f2480de5378e4dd4fcd1104663d0628
lilycato <146873958+lilycato@users.noreply.github.com>
2025-02-13 14:21:04 +0800

    Update lotto.py
    minor visual changes

--- lotto.py
+++ lotto.py
@@ -16,8 +16,9 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
            29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
            42, 43, 44, 45, 46, 47, 48, 49]
     
-list_box = tk.Listbox(font=('Arial', 15))
+list_box = tk.Listbox()
 list_box.place(relx=0.6, rely=0.5, anchor='center', height=300)
+list_box.configure(background='skyblue4', foreground='white', font=('Arial', 15))
 
 def lotto_gen_7():
     pick = random.sample(numbers, 7)
